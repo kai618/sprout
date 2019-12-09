0f5d717148353dadee51327903ffb9dc64994537
Hieu Pham <vanhieu1601@gmail.com>
2019-12-09 11:55:16 +0700

    website is hosted on firebase

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { WatchComponent } from './components/watch/watch.component';
+import { UploadComponent } from './components/upload/upload.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "upload", component: UploadComponent },
+  { path: "watch/:vid", component: WatchComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
